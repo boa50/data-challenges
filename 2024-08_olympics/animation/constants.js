69f8ded0ad4d83006af5508ed779d20db1a04a52
boa50 <bruno.albuquerque50@gmail.com>
2024-08-19 15:04:12 -0300

    Deleted unused file

--- 2024-08_olympics/animation/constants.js
+++
@@ -1,8 +0,0 @@
-// export const width = 600
-// export const margin = ({ top: 16, right: 6, bottom: 6, left: 72 })
-// export const duration = 250
-// export const k = 50
-// export const barSize = 36
-// export const n = 10
-// export const height = margin.top + barSize * n + margin.bottom
-// export const flagWidth = 40
