e207ad198cd8786d1e693ddbebf2ce0b81d6adf3
boa50 <bruno.albuquerque50@gmail.com>
2024-09-03 18:06:07 -0300

    Created the first version of the stacked bar chart

--- 2024-09_stacked_chart/chart.js
+++ 2024-09_stacked_chart/chart.js
@@ -1,37 +1,81 @@
-export const addChart = async (chartProps) => {
+import { addAxis, colours } from "../node_modules/visual-components/index.js"
+
+const includedFoods = ["Beef (beef herd)", "Lamb & Mutton", "Dark Chocolate",
+    "Cheese", "Shrimps (farmed)", "Coffee", "Fish (farmed)", "Pig Meat", "Palm Oil",
+    "Eggs", "Rice", "Nuts", "Milk", "Tofu", "Oatmeal", "Cassava", "Soy milk", "Bananas"]
+
+export const addChart = async (chartProps, theme = 'light') => {
     const { chart, width, height } = chartProps
+    const palette = theme === 'light' ? colours.paletteLightBg : colours.paletteDarkBg
     const data = await d3.csv('data/dataset.csv')
-        .then(data => data.map(d => {
-            return {
-                ...d,
-                landUse: +d.landUse,
-                farm: +d.farm,
-                animalFeed: +d.animalFeed,
-                others: +d.others
-            }
-        }))
+        .then(data => data
+            .filter(d => includedFoods.includes(d.food))
+            .map(d => {
+                return {
+                    ...d,
+                    landUse: convertNumber(d.landUse),
+                    farm: convertNumber(d.farm),
+                    animalFeed: convertNumber(d.animalFeed),
+                    others: convertNumber(d.others)
+                }
+            })
+            .map(d => { return { ...d, total: d.landUse + d.farm + d.animalFeed + d.others } })
+            .sort((a, b) => b.total - a.total)
+        )
+
+    const subgroups = ['landUse', 'farm', 'animalFeed', 'others']
+
+    const x = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, d => d.total) * 1.05])
+        .range([0, width])
+
+    const y = d3
+        .scaleBand()
+        .domain(data.map(d => d.food))
+        .range([0, height])
+        .padding(.1)
+
+    const colour = d3
+        .scaleOrdinal()
+        .domain(subgroups)
+        .range([palette.vermillion, palette.blue, palette.amber, palette.bluishGreen])
+
+    const stackedData = d3
+        .stack()
+        .keys(subgroups)
+        (data)
+
+    chart
+        .append('g')
+        .selectAll('g')
+        // Enter in the stack data = loop key per key = group per group
+        .data(stackedData)
+        .join('g')
+        .attr('fill', d => colour(d.key))
+        .attr('stroke', 'white')
+        .selectAll('rect')
+        // enter a second time = loop subgroup per subgroup to add all rectangles
+        .data(d => d)
+        .join('rect')
+        .attr('y', d => y(d.data.food))
+        .attr('x', d => x(d[0]))
+        .attr('width', d => x(d[1]) - x(d[0]))
+        .attr('height', y.bandwidth())
+
+    console.log(data.map(d => d.food));
 
 
-    // const x = d3
-    //     .scaleLinear()
-    //     .domain(d3.extent(data, d => d.X_VARIABLE).map((d, i) => d * [0.95, 1.05][i]))
-    //     .range([0, width])
-
-    // const y = d3
-    //     .scaleLinear()
-    //     .domain(d3.extent(data, d => d.Y_VARIABLE).map((d, i) => d * [0.95, 1.05][i]))
-    //     .range([height, 0])
-
-    // const x = d3
-    //     .scaleLinear()
-    //     .domain([0, MAX_VALUE])
-    //     .range([0, width])
-
-    // const y = d3
-    //     .scaleBand()
-    //     .domain(data.map(d => d.GROUP_FIELD))
-    //     .range([0, height])
-    //     .padding(.1)
+    addAxis({
+        chart,
+        width,
+        height,
+        colour: palette.axis,
+        x,
+        y,
+        hideXdomain: true,
+        hideYdomain: true
+    })
 
     // chart
     //     .selectAll('.myRect')
@@ -42,4 +86,8 @@ export const addChart = async (chartProps) => {
     //     .attr('width', d => x(d.VALUE))
     //     .attr('height', y.bandwidth())
     //     .attr('fill', '#69b3a2')
+}
+
+function convertNumber(number) {
+    return +number > 0 ? +number : 0
 }
--- 2024-09_stacked_chart/script.js
+++ 2024-09_stacked_chart/script.js
@@ -1,8 +1,8 @@
-import { appendChartContainer, getChart } from "../node_modules/visual-components/index.js"
+import { appendChartContainer, getChart, getMargin } from "../node_modules/visual-components/index.js"
 import { addChart } from "./chart.js"
 
 const chartId = appendChartContainer({ idNum: 0, chartTitle: 'Stacked' })
 
 await new Promise(r => setTimeout(r, 1))
 
-addChart(getChart({ id: chartId }))
+addChart(getChart({ id: chartId, margin: getMargin({ left: 128 }) }))
